4ca5d2ca721dcc03b1e1d2fc5a29b7972bd2543f
Halim <book1001.db@gmail.com>
2026-01-07 02:09:07 -0500

    updated

--- sheets.js
+++ sheets.js
@@ -1,3 +1,3 @@
-const projectUrl = "https://script.google.com/macros/s/AKfycbz3NS1RDb30YgFthzOhSzHZ3Vv59MZ9I455KQkgvnskm1DHc6hZ0Xr2hZKuEkdaQ2iF/exec";
+const projectUrl = "https://script.google.com/macros/s/AKfycbzqm2u0D6u4jEJ-62od6sXxdht2wWQZDNtmxFWacP5Peawzso-mWw87OaJ5nDpv0Evb/exec";
 
-const pressUrl = "https://script.google.com/macros/s/AKfycbwC3_aKt0-GxwwjxKom7e352eu05m6o_dLw4qV4xkwaNTUZ4QTQeWYstHORyaDWABMicA/exec"; 
+const pressUrl = "https://script.google.com/macros/s/AKfycbyOaaxXrdGs4Ujzgnh379KQ5nW-ISm2gn1i5Jf1hxCoAmEgVIUSNCdF8bnASfptGEvTsA/exec"; 
