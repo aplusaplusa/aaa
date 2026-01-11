ad930f7523c940068c6deba618a38bbdc14f5c3a
Halim <book1001.db@gmail.com>
2026-01-11 16:47:08 -0500

    updated

--- sheets.js
+++ sheets.js
@@ -1,3 +1,3 @@
-const projectUrl = "https://script.google.com/macros/s/AKfycbzqm2u0D6u4jEJ-62od6sXxdht2wWQZDNtmxFWacP5Peawzso-mWw87OaJ5nDpv0Evb/exec";
+const projectUrl = "https://script.google.com/macros/s/AKfycby0_YQNB0UWfbK6XscLCnuVFcHeLEgx76PTWbb3a0Ffsjk_VI9Ql8m2qvkvZQOFVigG/exec";
 
 const pressUrl = "https://script.google.com/macros/s/AKfycbyOaaxXrdGs4Ujzgnh379KQ5nW-ISm2gn1i5Jf1hxCoAmEgVIUSNCdF8bnASfptGEvTsA/exec"; 
